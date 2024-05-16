2062b4d35cfde2183835020d6f985d14f9d986b5
Navaneetha Krishnan Ramesh <118521309+Navaneethan1209@users.noreply.github.com>
2024-05-16 18:23:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import NavBar from './HeadFoot/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>     
+        <h1>TravelApp</h1>
+        
+    </div>
+  );
+}
+
+export default App;
